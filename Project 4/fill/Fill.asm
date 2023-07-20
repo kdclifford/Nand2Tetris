5cf6a70d83b06659df4e359f5bea6e4e687497f0
kyle clifford <kdclifford@icloud.com>
2023-07-20 15:15:38 +0100

    FIll script added

--- Project 4/fill/Fill.asm
+++ Project 4/fill/Fill.asm
@@ -12,3 +12,85 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+//Intial Setup
+@SCREEN
+D=A-1
+@address
+M=D
+
+
+(CLEAR)
+@SCREEN
+D=A-1
+@address
+M=D
+@counter
+M=0
+@CLEARLOOP
+0;JMP
+
+(FILL)
+@SCREEN
+D=A-1
+@addressClear
+M=D
+@counterClear
+M=0
+@LOOP
+0;JMP
+
+//Main Loop 
+(LOOP)
+@address
+D=M
+@24576
+D=D-A
+@KEY
+D;JEQ
+   @address
+   A=M
+   M=-1
+   @address
+   D=M
+   @1
+   D=D+A
+   @address
+   M=D
+   @counter
+   MD=M-1
+   @KEY
+   D;JGT
+
+
+//Clear Loop 
+(CLEARLOOP)
+@addressClear
+D=M
+@24576
+D=D-A
+@KEY
+D;JGT
+   @addressClear
+   A=M
+   M=0
+   @addressClear
+   D=M
+   @1
+   D=D+A
+   @addressClear
+   M=D
+   @counterClear
+   MD=M-1
+   @KEY
+   D;JGT
+
+
+//Key Pressed
+(KEY)
+@24576
+D=M
+@CLEAR
+D;JEQ
+@FILL
+0;JMP
