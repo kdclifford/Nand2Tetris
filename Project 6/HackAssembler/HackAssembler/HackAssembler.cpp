bb26aac11cd64fcef7b5eb98839c989fa52fc65c
kyle clifford <kdclifford@icloud.com>
2023-07-28 15:20:05 +0100

    add asm working

--- Project 6/HackAssembler/HackAssembler/HackAssembler.cpp
+++ Project 6/HackAssembler/HackAssembler/HackAssembler.cpp
@@ -9,106 +9,114 @@
 #include <argparse.hpp>
 #include <unordered_map>
 #include <bitset>
+#include <stdexcept>
 
+bool isNumeric(const std::string& input) {
+	try {
+		size_t pos;
+		std::stod(input, &pos);
+		return pos == input.length(); // Check if entire string was successfully converted
+	}
+	catch (std::invalid_argument&) {
+		return false; // Conversion failed
+	}
+	catch (std::out_of_range&) {
+		return false; // Number is out of range
+	}
+}
 
 int main(int argc, char* argv[])
 {
-	typedef struct bits3
+	typedef struct instruction
 	{
-		unsigned int v1 : 3;  // 1st 3 bits (in 1st byte) are b1
-		unsigned int : 5;  // padding
+		unsigned int start : 3;  // padding
+		//unsigned int a : 1;  // a
+		unsigned int c : 7;  // comp
+		unsigned int d : 3;  // dest
+		unsigned int j : 3;  // jump
 	};
 
-	const std::unordered_map<std::string, std::string> predefined = {
-	{"R0",	   "0000000000000000"},
-	{"R1",	   "0000000000000001"},
-	{"R2",	   "0000000000000010"},
-	{"R3",	   "0000000000000011"},
-	{"R4",	   "0000000000000100"},
-	{"R5",	   "0000000000000101"},
-	{"R6",	   "0000000000000110"},
-	{"R7",	   "0000000000000111"},
-	{"R8",	   "0000000000001000"},
-	{"R9",	   "0000000000001001"},
-	{"R10",	   "0000000000001010"},
-	{"R11",	   "0000000000001011"},
-	{"R12",	   "0000000000001100"},
-	{"R13",	   "0000000000001101"},
-	{"R14",	   "0000000000001110"},
-	{"R15",	   "0000000000001111"},
-	{"SP",	   "0000000000000000"},
-	{"LCL",	   "0000000000000001"},
-	{"ARG",	   "0000000000000010"},
-	{"THIS",   "0000000000000011"},
-	{"THAT",   "0000000000000100"},
-	{"SCREEN", "0100000000000000"},
-	{"KDB",	   "0110000000000000"}
+	const std::unordered_map<std::string, uint16_t> predefined = {
+	{"R0",	    {0b0}},
+	{"R1",	    {0b1}},
+	{"R2",	    {0b10}},
+	{"R3",	    {0b11}},
+	{"R4",	    {0b100}},
+	{"R5",	    {0b101}},
+	{"R6",	    {0b110}},
+	{"R7",	    {0b111}},
+	{"R8",	    {0b1000}},
+	{"R9",		{0b1001}},
+	{"R10",		{0b1010}},
+	{"R11",		{0b1011}},
+	{"R12",		{0b1100}},
+	{"R13",		{0b1101}},
+	{"R14",		{0b1110}},
+	{"R15",		{0b1111}},
+	{"SP",	    {0b0}},
+	{"LCL",		{0b1}},
+	{"ARG",		{0b10}},
+	{"THIS",    {0b11}},
+	{"THAT",    {0b100}},
+	{"SCREEN",  {0b100'0000'0000'0000}},
+	{"KDB",		{0b110'0000'0000'0000}}
 	};
 
-	const std::unordered_map<std::string, std::string> comp = {
-	{"0",	   "0101010"},
-	{"1",	   "0111111"},
-	{"-1",	   "0111010"},
-	{"D",	   "0001100"},
-	{"A",	   "0110000"},
-	{"M",	   "1110000"},
-	{"!D",	   "0001101"},
-	{"!A",	   "0110001"},
-	{"!M",	   "1110001"},
-	{"-D",	   "0001111"},
-	{"-A",	   "0110011"},
-	{"-M",	   "1110011"},
-	{"D+1",	   "0011111"},
-	{"A+1",	   "0110111"},
-	{"M+1",	   "1110111"},
-	{"D-1",	   "0001110"},
-	{"A-1",	   "0110010"},
-	{"M-1",	   "1110010"},
-	{"D+A",	   "0000010"},
-	{"D+M",	   "1000010"},
-	{"D-A",	   "0010011"},
-	{"D-M",	   "1010011"},
-	{"A-D",	   "0000111"},
-	{"M-D",	   "1000111"},
-	{"D&A",	   "0000000"},
-	{"D&M",	   "1000000"},
-	{"D|A",	   "0010101"},
-	{"D|M",	   "A010101"}
+	std::unordered_map<std::string, uint16_t> otherAddresses;
+
+
+	const std::unordered_map<std::string, uint8_t> comp = {
+	{"0",	   {0b10'1010}},
+	{"1",	   {0b11'1111}},
+	{"-1",	   {0b11'1010}},
+	{"D",	   {0b1100}},
+	{"A",	   {0b11'0000}},
+	{"M",	   {0b111'0000}},
+	{"!D",	   {0b1101}},
+	{"!A",	   {0b11'0001}},
+	{"!M",	   {0b111'0001}},
+	{"-D",	   {0b1111}},
+	{"-A",	   {0b11'0011}},
+	{"-M",	   {0b111'0011}},
+	{"D+1",	   {0b1'1111}},
+	{"A+1",	   {0b11'0111}},
+	{"M+1",	   {0b111'0111}},
+	{"D-1",	   {0b1'110}},
+	{"A-1",	   {0b11'0010}},
+	{"M-1",	   {0b111'0010}},
+	{"D+A",	   {0b10}},
+	{"D+M",	   {0b100'0010}},
+	{"D-A",	   {0b1'0011}},
+	{"D-M",	   {0b101'0011}},
+	{"A-D",	   {0b111}},
+	{"M-D",	   {0b100'0111}},
+	{"D&A",	   {0b0}},
+	{"D&M",	   {0b100'0000}},
+	{"D|A",	   {0b1'0101}},
+	{"D|M",	   {0b101'0101}}
 	};
 
-	//const std::unordered_map<std::string, bits3> dest = {
-	//{"null",   "000"},
-	//{"M",	   "001"},
-	//{"D",	   "010"},
-	//{"DM",	   "011"},
-	//{"A",	   "100"},
-	//{"AM",	   "101"},
-	//{"AD",	   "110"},
-	//{"ADM",	   "111"}
-	//};
+	const std::unordered_map<std::string, uint8_t> dest = {
+	{"null",   {0b0}},
+	{"M",	   {0b1}},
+	{"D",	   {0b10}},
+	{"DM",	   {0b11}},
+	{"A",	   {0b100}},
+	{"AM",	   {0b101}},
+	{"AD",	   {0b110}},
+	{"ADM",	   {0b111}}
+	};
 
-	//const std::unordered_map<std::string, bits3> jump = {
-	//{"null",   "000"},
-	//{"JGT",	   "001"},
-	//{"JEQ",	   "010"},
-	//{"JGE",	   "011"},
-	//{"JLT",	   "100"},
-	//{"JNE",	   "101"},
-	//{"JLE",	   "110"},
-	//{"JMP",	   "111"}
-	//};
-
-
-
-
-	std::string s = std::bitset< 64 >(12345).to_string(); // string conversion
-
-	std::cout << std::bitset< 64 >(54321) << ' '; // direct output
-
-	std::bitset< 64 > input;
-	std::cin >> input;
-	unsigned long ul = input.to_ulong();
-
+	const std::unordered_map<std::string, uint8_t> jump = {
+	{"null",   {0b0}},
+	{"JGT",	   {0b1}},
+	{"JEQ",	   {0b10}},
+	{"JGE",	   {0b11}},
+	{"JLT",	   {0b100}},
+	{"JNE",	   {0b101}},
+	{"JLE",	   {0b110}},
+	{"JMP",	   {0b111}}
+	};
 
 	//Basic
 	const std::string k_add = "../../add/Add.asm";
@@ -126,8 +134,9 @@ int main(int argc, char* argv[])
 
 	program.add_argument("-f", "--filePath")
 		.help("File path to asm file")
-		.nargs(1)
-		.required();
+		.default_value(k_add)
+		.nargs(1);
+		//.required();
 
 	//Try pass args
 	try {
@@ -158,10 +167,72 @@ int main(int argc, char* argv[])
 			std::string whiteSpcae;
 			while (std::getline(ss, whiteSpcae, ' '))
 			{
-				//commands.push(whiteSpcae);
+
+				if (whiteSpcae[0] == '@')
+				{
+					const std::string key = whiteSpcae.substr(1);
+
+					const auto address = predefined.find(key);
+
+					if (address != predefined.end())
+					{
+						std::cout << std::bitset<16>(address->second) << std::endl;
+					}
+					else
+					{
+						const auto address2 = otherAddresses.find(key);
+						if (address2 == otherAddresses.end())
+						{
+							if (isNumeric(key))
+							{
+								std::cout << std::bitset<16>(std::stoi(key)) << std::endl;
+							}
+							else
+							{
+								const int value = (15 + otherAddresses.size());
+
+								std::pair<std::string, uint16_t> newAdress(key, value);
+
+								otherAddresses.insert(newAdress);
+
+								std::cout << "new address added: " << std::bitset<16>(newAdress.second) << std::endl;
+							}
+
+						}
+					}
+				}
+						else
+						{
+							const auto test = predefined.find(whiteSpcae);
+
+							std::string comp2 = "";
+							std::string dest2 = "";
+
+							bool equal = false;
+							// Returns first token
+							for (const auto letter : whiteSpcae)
+							{
+								if (letter == '=')
+								{
+									equal = true;
+								}
+								else if (equal)
+								{
+									comp2 += letter;
+								}
+								else
+								{
+									dest2 += letter;
+								}
+							}
+
+
+							instruction binary = { {0b111},	{comp.find(comp2)->second},	{dest.find(dest2)->second}, {0b000}};
+							std::cout << std::bitset<3>(binary.start) << std::bitset<7>(binary.c) << std::bitset<3>(binary.d) << std::bitset<3>(binary.j) << std::endl;
+						}
+
 			}
 		}
-
 	}
 
 	myfile.close();
