f1cb8ad2b3673cc5718b9f3f46cd734a71e8ccf8
kyle clifford <kdclifford@icloud.com>
2023-07-31 12:43:12 +0100

    White space help functions added and isnumeric updated to better impl

--- Project 6/HackAssembler/HackAssembler/HelperFuncs.h
+++ Project 6/HackAssembler/HackAssembler/HelperFuncs.h
@@ -1,21 +1,70 @@
 #pragma once
-#include <sstream>
-
+#include <cassert>
+#include <charconv>
+#include <iomanip>
+#include <iostream>
+#include <optional>
+#include <string_view>
+#include <system_error>
+#include <algorithm> 
+#include <functional> 
+#include <cctype>
+#include <locale>
 
 namespace Helpers
 {
+    static inline bool isNumeric(const std::string_view& input) {
+        
+        std::cout << "String: " << std::quoted(input) << ". ";
+        int result{};
+        auto [ptr, ec] = std::from_chars(input.data(), input.data() + input.size(), result);
+        
+        if (ec == std::errc())
+        {
+            std::cout << "Result: " << result << ", ptr -> " << std::quoted(ptr) << '\n';
+            return true;
+        }
+        else if (ec == std::errc::invalid_argument)
+        {
+            std::cout << "That isn't a number.\n";
+        }
+        else if (ec == std::errc::result_out_of_range)
+        {
+            std::cout << "This number is larger than an int.\n";
+        }
+        return false;
+    }
 
-	bool isNumeric(const std::string& input) {
-		try {
-			size_t pos;
-			std::stod(input, &pos);
-			return pos == input.length(); // Check if entire string was successfully converted
-		}
-		catch (std::invalid_argument&) {
-			return false; // Conversion failed
-		}
-		catch (std::out_of_range&) {
-			return false; // Number is out of range
-		}
-	}
+    // trim from start (in place)
+    static inline std::string& ltrim(std::string& s) {
+        s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+            [](int c) {return !std::isspace(c); }));
+        return s;
+    }
+
+    // trim from end (in place)
+    static inline std::string& rtrim(std::string& s) {
+        s.erase(std::find_if(s.rbegin(), s.rend(),
+            [](int c) { return !std::isspace(c); }).base(), s.end());
+        return s;
+    }
+
+    // trim from both ends (in place)
+    static inline void trim(std::string& s) {
+        rtrim(s);
+        ltrim(s);
+    }
+
+    // trim from start (copying)
+    static inline std::string ltrim_copy(std::string s) {
+        ltrim(s);
+        return s;
+    }
+
+    // trim from end (copying)
+    static inline std::string rtrim_copy(std::string s) {
+        rtrim(s);
+        return s;
+    }
+
 }
